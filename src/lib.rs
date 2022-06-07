ba57a004a2e32602ec13739472b7fa134fbebbbd
Alberto Faria <afaria@redhat.com>
2022-06-07 22:58:41 +0100

    Prefix feature "unsafe-op-in-unsafe-fn" with an underscore
    To make it clearer that it is "private" and unstable, and that it should
    not be enabled explicitly by dependent crates.
    
    Signed-off-by: Alberto Faria <afaria@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(feature = "unsafe-op-in-unsafe-fn", deny(unsafe_op_in_unsafe_fn))]
-#![cfg_attr(not(feature = "unsafe-op-in-unsafe-fn"), allow(unused_unsafe))]
+#![cfg_attr(feature = "_unsafe-op-in-unsafe-fn", deny(unsafe_op_in_unsafe_fn))]
+#![cfg_attr(not(feature = "_unsafe-op-in-unsafe-fn"), allow(unused_unsafe))]
 
 use bitflags::bitflags;
 use std::io::Error;
