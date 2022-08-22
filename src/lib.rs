3d649dd18e40c55e0624efa4876b8c7a934ac751
Alberto Faria <afaria@redhat.com>
2022-08-23 00:55:32 +0100

    virtio-driver: Move transport modules into a 'transports' module
    Signed-off-by: Alberto Faria <afaria@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,22 @@
 
 use bitflags::bitflags;
 use std::io::Error;
-use std::os::unix::io::RawFd;
-use std::rc::Rc;
 
+mod transports;
 mod util;
 mod virtio_blk;
 pub mod virtqueue;
 
-#[cfg(feature = "vhost-user")]
-mod vhost_user;
-#[cfg(feature = "vhost-vdpa")]
-mod vhost_vdpa;
-
+pub use transports::*;
 pub use util::bytevalued::*;
 pub use util::endian::*;
 pub use util::eventfd::*;
 pub use util::sock_ctrl_msg::*;
 
-#[cfg(feature = "vhost-user")]
-pub use vhost_user::VhostUser;
-#[cfg(feature = "vhost-vdpa")]
-pub use vhost_vdpa::VhostVdpa;
-
 pub use virtio_blk::{
     validate_lba, VirtioBlkConfig, VirtioBlkFeatureFlags, VirtioBlkQueue, VirtioBlkReqBuf,
     VirtioBlkTransport,
 };
-use virtqueue::{Virtqueue, VirtqueueLayout};
 
 bitflags! {
     pub struct VirtioFeatureFlags: u64 {
@@ -58,55 +47,4 @@ pub struct Completion<C> {
 
     /// 0 on success, a negative `errno` value on error.
     pub ret: i32,
-}
-
-/// An interface to the virtio transport/bus of a device
-///
-/// Type parameters:
-/// - `C` represents the device configuration space, as returned by [`VirtioTransport::get_config`];
-/// - `R` has the same meaning as in [`Virtqueue`], and is used to store device-specific per-request
-///   data.
-pub trait VirtioTransport<C: ByteValued, R: Copy> {
-    /// Returns the maximum number of queues supported by the device.
-    fn max_queues(&self) -> usize;
-
-    /// Returns the maximum number of memory regions supported by the transport.
-    fn max_mem_regions(&self) -> u64;
-
-    /// Allocates or maps the memory to store the virtqueues in.
-    ///
-    /// This memory must be accessible by the device and is also used for additional per-request
-    /// metadata (such as request headers) that is created internally by the driver and must be
-    /// visible for the device.
-    fn alloc_queue_mem(&mut self, layout: &VirtqueueLayout) -> Result<&mut [u8], Error>;
-
-    /// Maps a memory region with the transport.
-    ///
-    /// Requests to the device may only refer to memory that is in a mapped memory region.
-    fn map_mem_region(
-        &mut self,
-        addr: usize,
-        len: usize,
-        fd: RawFd,
-        fd_offset: i64,
-    ) -> Result<(), Error>;
-
-    /// Unmaps a memory region from the transport.
-    fn unmap_mem_region(&mut self, addr: usize, len: usize) -> Result<(), Error>;
-
-    /// Initialises and enables the passed queues on the transport level.
-    fn setup_queues(&mut self, queues: &[Virtqueue<R>]) -> Result<(), Error>;
-
-    /// Returns the negotiated virtio feature flags.
-    fn get_features(&self) -> u64;
-
-    /// Queries the device configuration.
-    fn get_config(&mut self) -> Result<C, Error>;
-
-    /// Returns an [`EventFd`] that can be written to notify the device of new requests in the
-    /// queue.
-    fn get_submission_fd(&self, queue_idx: usize) -> Rc<EventFd>;
-
-    /// Returns an [`EventFd`] that can be read to be notified of request completions in the queue.
-    fn get_completion_fd(&self, queue_idx: usize) -> Rc<EventFd>;
 }
---
+++ src/transports/mod.rs
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: (MIT OR Apache-2.0)
+
+use std::io::Error;
+use std::os::unix::io::RawFd;
+use std::rc::Rc;
+
+#[cfg(feature = "vhost-user")]
+mod vhost_user;
+#[cfg(feature = "vhost-vdpa")]
+mod vhost_vdpa;
+
+#[cfg(feature = "vhost-user")]
+pub use vhost_user::VhostUser;
+#[cfg(feature = "vhost-vdpa")]
+pub use vhost_vdpa::VhostVdpa;
+
+use crate::util::bytevalued::ByteValued;
+use crate::util::eventfd::EventFd;
+use crate::virtqueue::{Virtqueue, VirtqueueLayout};
+
+/// An interface to the virtio transport/bus of a device
+///
+/// Type parameters:
+/// - `C` represents the device configuration space, as returned by [`VirtioTransport::get_config`];
+/// - `R` has the same meaning as in [`Virtqueue`], and is used to store device-specific per-request
+///   data.
+pub trait VirtioTransport<C: ByteValued, R: Copy> {
+    /// Returns the maximum number of queues supported by the device.
+    fn max_queues(&self) -> usize;
+
+    /// Returns the maximum number of memory regions supported by the transport.
+    fn max_mem_regions(&self) -> u64;
+
+    /// Allocates or maps the memory to store the virtqueues in.
+    ///
+    /// This memory must be accessible by the device and is also used for additional per-request
+    /// metadata (such as request headers) that is created internally by the driver and must be
+    /// visible for the device.
+    fn alloc_queue_mem(&mut self, layout: &VirtqueueLayout) -> Result<&mut [u8], Error>;
+
+    /// Maps a memory region with the transport.
+    ///
+    /// Requests to the device may only refer to memory that is in a mapped memory region.
+    fn map_mem_region(
+        &mut self,
+        addr: usize,
+        len: usize,
+        fd: RawFd,
+        fd_offset: i64,
+    ) -> Result<(), Error>;
+
+    /// Unmaps a memory region from the transport.
+    fn unmap_mem_region(&mut self, addr: usize, len: usize) -> Result<(), Error>;
+
+    /// Initialises and enables the passed queues on the transport level.
+    fn setup_queues(&mut self, queues: &[Virtqueue<R>]) -> Result<(), Error>;
+
+    /// Returns the negotiated virtio feature flags.
+    fn get_features(&self) -> u64;
+
+    /// Queries the device configuration.
+    fn get_config(&mut self) -> Result<C, Error>;
+
+    /// Returns an [`EventFd`] that can be written to notify the device of new requests in the
+    /// queue.
+    fn get_submission_fd(&self, queue_idx: usize) -> Rc<EventFd>;
+
+    /// Returns an [`EventFd`] that can be read to be notified of request completions in the queue.
+    fn get_completion_fd(&self, queue_idx: usize) -> Rc<EventFd>;
+}
